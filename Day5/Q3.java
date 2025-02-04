4abdd495ec004a3d8ecaea37bcbadd6da07ec7cf
Avinash <avinashlpu75@gmail.com>
2025-02-04 19:29:08 +0530

    "New Day"

--- Day5/Q3.java
+++ Day5/Q3.java
@@ -1,3 +1,20 @@
+/* 216 Contain duplicate II */
+
+import java.util.HashMap;
+
 public class Q3 {
-    
+  
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> avinash = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(avinash.containsKey(nums[i])){
+                if(i - avinash.get(nums[i]) <= k){
+                    return true;
+                }
+            }
+            avinash.put(nums[i], i);
+        }
+        return false;
+    }
 }
+
