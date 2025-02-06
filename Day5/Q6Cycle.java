cdf4ea46268d08234173d0bd2b4edfa3bf453f6c
Avinash <avinashlpu75@gmail.com>
2025-02-06 15:32:04 +0530

    "Sort"

---
+++ Day5/Q6Cycle.java
@@ -0,0 +1,27 @@
+public class Q6Cycle {
+    public static void main(String[] args){
+        int[] arr = {5,7,3,2,4,1,2,6};
+        cycle(arr);
+        System.out.println(arr);
+    }
+
+    static void cycle(int[] arr){
+        int i=0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int last){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+}
+
