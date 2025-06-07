635b823a5e86d21dd6f762735a62946e585884ff
Avinash Singh <avinashlpu75@gmail.com>
2025-06-07 23:24:55 +0530

    Time: 5 ms (8.27%), Space: 45.1 MB (6.97%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,14 @@
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> visited = new HashSet<>();
+        ListNode current = head;
+
+        while (current != null) {
+            if (visited.contains(current)) return true;
+            visited.add(current);
+            current = current.next;
+        }
+        return false;
+    }
+}
