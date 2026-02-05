5eeb8633ca7c3786cb9271a9c42c688e9a79c72e
Avinash Singh <avinashlpu75@gmail.com>
2026-02-05 09:23:00 +0530

    Time: 1 ms (98.72%), Space: 46.6 MB (86.54%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                int target = (i + nums[i] % n + n) % n;
+                result[i] = nums[target];
+            }
+        }
+        return result;
+    }
+}
